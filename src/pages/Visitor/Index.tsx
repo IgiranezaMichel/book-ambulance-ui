3a05bcf3cc8940b860fc53752043a98a0c8ca130
IgiranezaMichel <igiramichael@gmail.com>
2023-12-11 13:34:00 +0200

    add all

---
+++ src/pages/Visitor/Index.tsx
@@ -0,0 +1,26 @@
+const Index=()=>{
+    return(
+        <>
+        <img className=""  src="/parchment.png" alt=""/>
+    {/* <div className="container-fluid pt-5 bg-primary mb-5 " style={{height:"100dvh"}}>
+        <div className="container pt-5">
+        <img className="card-img"  src="/caring-nurse-patient-generative-ai_94628-4193.jpg" alt=""/>
+            <div className="row g-5 pt-5 card-img-overlay">
+                <div className="col-lg-6 align-self-center text-center text-lg-start mb-lg-5">
+                    <div className="btn btn-sm border rounded-pill text-white px-3 mb-3 animated slideInRight">AI.Tech</div>
+                    <h1 className="display-4 text-white mb-4 animated slideInRight">Find Hospital Ambulance online</h1>
+                    <p className="text-white mb-4 animated slideInRight">Tempor rebum no at dolore lorem clita rebum rebum ipsum
+                        rebum stet dolor sed justo kasd. Ut dolor sed magna dolor sea diam. Sit diam sit</p>
+                    <a href="" className="btn btn-light py-sm-3 px-sm-5 rounded-pill me-3 animated slideInRight">Read More</a>
+                    <a href="" className="btn btn-outline-light py-sm-3 px-sm-5 rounded-pill animated slideInRight">Contact Us</a>
+                </div>
+                    
+            </div>
+        </div>
+    </div>
+    <img className=""  src="/vite.svg" alt=""/>
+    <img className=""  src="/parchment.png" alt=""/> */}
+        </>
+    )
+}
+export default Index
