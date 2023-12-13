ec6a0a809bc24c807f9c2430ee62d799894f9cbb
IgiranezaMichel <igiramichael@gmail.com>
2023-12-13 21:42:23 +0200

    add /,/signup , /login

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import SignUp from './pages/Visitor/SignUp'
 import { Admin } from './pages/Admin/Admin'
 import { CrudHospital } from './Component/Admin/CrudHospital'
 import UserHome from './pages/User/UserHome'
+import { DriverHome } from './pages/Driver/DriverHome'
+import Index from './pages/Visitor/Index'
+import Login from './pages/Visitor/Login'
 
 function App() {
   return (
     <>
  <Router>
-  <Routes>
-    <Route path='/admin' element={<Admin/>}/>
+    <Routes>
+      <Route path='/' element={<Index/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/admin' element={<Admin/>}/>
       <Route path='/admin/hospital/:id/:action' element={<CrudHospital/>}/>
-      <Route path='/' element={<SignUp/>}/>
       <Route path='/user' element={<UserHome/>}/>
+      <Route path='/driver' element={<DriverHome/>}/>
     </Routes>
  </Router>
     </>
