8bd546eac59af79ac7c77991f531080339eed0da
IgiranezaMichel <igiramichael@gmail.com>
2023-12-13 06:55:14 +0200

    remove QueryExample

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import SignUp from './pages/Visitor/SignUp'
-import QueryExample from './pages/Visitor/QueryExample'
 import { Admin } from './pages/Admin/Admin'
 import { CrudHospital } from './Component/Admin/CrudHospital'
 
@@ -12,7 +11,6 @@ function App() {
   <Route path='/admin' element={<Admin/>}/>
   <Route path='/admin/hospital/:id/:action' element={<CrudHospital/>}/>
     <Route path='/' element={<SignUp/>}/>
-    <Route path='/q' element={<QueryExample/>}/>
   </Routes>
  </Router>
     </>
