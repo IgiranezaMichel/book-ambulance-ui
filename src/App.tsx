3eedb9671fc7d6c7d0f42a1a6d1195d530138282
IgiranezaMichel <igiramichael@gmail.com>
2023-12-13 16:07:59 +0200

    Add UserHome

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import SignUp from './pages/Visitor/SignUp'
 import { Admin } from './pages/Admin/Admin'
 import { CrudHospital } from './Component/Admin/CrudHospital'
+import UserHome from './pages/User/UserHome'
 
 function App() {
   return (
     <>
  <Router>
   <Routes>
-  <Route path='/admin' element={<Admin/>}/>
-  <Route path='/admin/hospital/:id/:action' element={<CrudHospital/>}/>
-    <Route path='/' element={<SignUp/>}/>
-  </Routes>
+    <Route path='/admin' element={<Admin/>}/>
+      <Route path='/admin/hospital/:id/:action' element={<CrudHospital/>}/>
+      <Route path='/' element={<SignUp/>}/>
+      <Route path='/user' element={<UserHome/>}/>
+    </Routes>
  </Router>
     </>
   )
